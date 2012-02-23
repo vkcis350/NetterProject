5c841f624dd1798ddc259307ccc635225c2f01b5
vkcis350 <vayu@sas.upenn.edu>
2012-02-23 06:29:16 -0500

    Removed StudentSelection.java, will add through Android Manifest later.

--- AttendanceApp/src/edu/upenn/cis350/AttendanceAppActivity.java
+++ AttendanceApp/src/edu/upenn/cis350/AttendanceAppActivity.java
@@ -2,7 +2,7 @@ package edu.upenn.cis350;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+//Test
 public class AttendanceAppActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
