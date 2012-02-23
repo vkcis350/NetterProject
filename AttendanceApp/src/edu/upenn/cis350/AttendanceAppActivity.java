c59f9329d8cf0d4dc62c203c39de1aa44590ddc1
vkcis350 <vayu@sas.upenn.edu>
2012-02-23 07:56:49 -0500

    Made first cut of login page which trivially accepts login if user's name is "admin" and rejects otherwise. If it accepts, it fires up StudentSelectionActivity.java (which should be changed as per our model, I just used this as an example).

--- AttendanceApp/src/edu/upenn/cis350/AttendanceAppActivity.java
+++ AttendanceApp/src/edu/upenn/cis350/AttendanceAppActivity.java
@@ -1,13 +1,46 @@
 package edu.upenn.cis350;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 //Test
 public class AttendanceAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	String userName=""; //access this from other activities to get info on user's credentials
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void onLoginClick(View v){
+		EditText user = (EditText) findViewById(R.id.user);
+		EditText pass = (EditText) findViewById(R.id.password);
+		
+		userName=user.getText().toString();
+		String pw = pass.getText().toString();
+		pass.setText("");
+		
+		if(validate(userName,pw)){
+			//MODIFY THE FOLLOWING LINE'S CLASS IN ORDER TO OPEN UP ANOTHER CLASS, I'VE JUST USED STUDENTSELECTIONACTIVITY.CLASS FOR EXAMPLE
+			Intent i = new Intent(this,StudentSelectionActivity.class);
+			startActivity(i);
+		}else{
+			Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+			
+		}
+	}
+
+	public void onQuitClick(View v){
+			finish();
+	}
+
+	private boolean validate(String user, String pass){
+		
+		return userName.equals("admin");//will be method which validates user's name and pw, currently returns true iff username input is "admin"
+	}
 }
