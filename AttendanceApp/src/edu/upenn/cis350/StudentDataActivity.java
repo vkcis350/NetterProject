7de3269df55ee0d3ba9af54c0d7236762e180ee3
vkcis350 <vayu@sas.upenn.edu>
2012-03-29 20:48:26 -0400

    StudentDataActivity

--- AttendanceApp/src/edu/upenn/cis350/StudentDataActivity.java
+++ AttendanceApp/src/edu/upenn/cis350/StudentDataActivity.java
@@ -5,20 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import edu.upenn.cis350.R;
+import edu.upenn.cis350.localstore.CheckinDataSource;
+import edu.upenn.cis350.localstore.SchoolActivityDataSource;
+import edu.upenn.cis350.localstore.StudentDataSource;
+import edu.upenn.cis350.models.Student;
 
 public class StudentDataActivity extends Activity{
+	Student curStudent;
+	StudentDataSource studentData; //database access object
 	//Need a student id from the last activity
-	
 	//Take that id and populate the student profile.
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.studentdata);
-		//getStudent(student_id);
-		//populateView(student); //check out studentdata.xml
+
+		Bundle extras = getIntent().getExtras();
+		long studentID = extras.getLong("STUDENT_ID");
+		openData();
+		populateFields();
+		curStudent=(Student) studentData.get(studentID);
 	}
 	
 	public void onSaveDataClick(View v){
@@ -33,5 +43,26 @@ public class StudentDataActivity extends Activity{
 		//back to last activity
 		setResult(RESULT_CANCELED);
 		finish();
+	}
+	
+	public void populateFields(){
+		EditText nameField = (EditText) findViewById(R.id.student_name_field);
+	}
+	
+	public void onPause()
+	{
+		super.onPause();
+		studentData.close();
+	}
+	
+	public void openData()
+	{
+		studentData = new StudentDataSource(this);
+		studentData.open();
+	}
+	
+	public void closeData()
+	{
+		studentData.close();
 	}
 }
