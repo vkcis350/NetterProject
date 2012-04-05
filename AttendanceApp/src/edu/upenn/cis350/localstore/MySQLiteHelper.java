f864bc8cc504b458f6fdc33f6c87d959713daccb
Xiao Li <lixiao1@seas.upenn.edu>
2012-04-05 19:16:42 -0400

    fixed bug that happens when opening the comments; seemed to be due getCheckedItemPosition() again

--- AttendanceApp/src/edu/upenn/cis350/localstore/MySQLiteHelper.java
+++ AttendanceApp/src/edu/upenn/cis350/localstore/MySQLiteHelper.java
@@ -58,7 +58,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 	public static final int CHECKINS_CHECKIN_LAST_CHANGE_INDEX = 6;
 	
 	private static final String DATABASE_NAME = "attendance.db";
-	private static final int DATABASE_VERSION = 83;
+	private static final int DATABASE_VERSION = 84;
 	
 	public MySQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
