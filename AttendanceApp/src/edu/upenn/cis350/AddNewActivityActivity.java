fe382e116abd0428d9529b07829ed08ec999a99d
jkodner05 <jkodner05@yahoo.com>
2012-04-06 19:16:49 -0400

    turns out lining up buttons crashes things. Reverted that. Also, popup menu when adding activity.

--- AttendanceApp/src/edu/upenn/cis350/AddNewActivityActivity.java
+++ AttendanceApp/src/edu/upenn/cis350/AddNewActivityActivity.java
@@ -4,9 +4,12 @@ import edu.upenn.cis350.localstore.SchoolActivityDataSource;
 import edu.upenn.cis350.models.Model;
 import edu.upenn.cis350.models.SchoolActivity;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -21,7 +24,7 @@ public class AddNewActivityActivity extends SyncableActivity {
 		setContentView(R.layout.add_new_activity);
 	}
 	
-	public void onCreateActivityClick(View v)
+	public void createActivity()
 	{
 		EditText activity_name = (EditText)findViewById(R.id.newActivityName);
     	String new_activity_name = activity_name.getText().toString();
@@ -34,6 +37,37 @@ public class AddNewActivityActivity extends SyncableActivity {
     	
     	Intent i = new Intent(this,ActivityListActivity.class);
 		startActivity(i);
+	}
+	
+	public void onCreateActivityClick(View v)
+	{
+		EditText activity_name = (EditText)findViewById(R.id.newActivityName);
+    	String new_activity_name = activity_name.getText().toString();
+		
+		AlertDialog mDialog = new AlertDialog.Builder(this)
+		.setTitle("Confirm New Activity")
+		.setMessage("Are you sure you want to create " + new_activity_name + "?")
+		.setPositiveButton("Yes", null)
+		.setNegativeButton("No", null)
+		.show();
+
+		WindowManager.LayoutParams layoutParams = mDialog.getWindow().getAttributes();
+		layoutParams.dimAmount = 0.9f;
+		mDialog.getWindow().setAttributes(layoutParams);
+		mDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+
+		//what happens when you press the buttons
+		mDialog.setButton("Yes", new DialogInterface.OnClickListener() {  
+			public void onClick(DialogInterface dialog, int which) {
+				createActivity();
+				Toast.makeText(getApplicationContext(), "Activity created!",
+						Toast.LENGTH_SHORT).show();
+			} });
+		mDialog.setButton2("No", new DialogInterface.OnClickListener() {  
+			public void onClick(DialogInterface dialog, int which) {  
+				Toast.makeText(getApplicationContext(), "Activity was not created.",
+						Toast.LENGTH_SHORT).show();
+			} });  
 	}
 	
 	public void onNotCreateClick(View v)
