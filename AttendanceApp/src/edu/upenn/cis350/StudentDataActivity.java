6bce22d95cd350cd126c86355f95b15bb0443653
jkodner05 <jkodner05@yahoo.com>
2012-04-28 11:46:05 -0400

    Included Screenshots under Docs/Screenshots. Moved the documentation document to Docs/. Changed interface for viewing student info

--- AttendanceApp/src/edu/upenn/cis350/StudentDataActivity.java
+++ AttendanceApp/src/edu/upenn/cis350/StudentDataActivity.java
@@ -1,4 +1,5 @@
 package edu.upenn.cis350;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import edu.upenn.cis350.R;
@@ -19,60 +20,77 @@ import edu.upenn.cis350.localstore.StudentDataSource;
 import edu.upenn.cis350.models.Checkin;
 import edu.upenn.cis350.models.Student;
 
-public class StudentDataActivity extends SyncableActivity{
+public class StudentDataActivity extends SyncableActivity {
 	Student curStudent;
 	Checkin lastAction;
-	StudentDataSource studentData; //database access object
-	//Need a student id from the last activity
-	//Take that id and populate the student profile.
-	CheckinDataSource checkinData; //database access object
+	StudentDataSource studentData; // database access object
+	// Need a student id from the last activity
+	// Take that id and populate the student profile.
+	CheckinDataSource checkinData; // database access object
 	SchoolActivityDataSource activityData;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.studentdata);
 
 		Bundle extras = getIntent().getExtras();
-		long studentID= extras.getLong("STUDENT_ID");
-		//long studentID=Long.parseLong(preStudentID);
-		Log.d("StudentDataActivity","Profile of Student with ID number"+studentID+"loaded");
+		long studentID = extras.getLong("STUDENT_ID");
+		// long studentID=Long.parseLong(preStudentID);
+		Log.d("StudentDataActivity", "Profile of Student with ID number"
+				+ studentID + "loaded");
 		openData();
 
-		curStudent=(Student) studentData.get(studentID);
+		curStudent = (Student) studentData.get(studentID);
 
 		lastAction = checkinData.getMostRecentForStudent(studentID);
 
-
-		if(curStudent==null){
-			Log.d("StudentDataActivity","Student object is null");
+		if (curStudent == null) {
+			Log.d("StudentDataActivity", "Student object is null");
 		}
 		populateFields();
 	}
 
+	public void onSaveDataClick(View v) {
+		// updatedStudentData = new Student(); fill with data from the form
+		// student = getStudent(student_id);
+		// Compare the two, if they are different then update the dbmodel with
+		// the info
+		setResult(RESULT_OK);
+		finish();
+	}
+
+	public void onCancelDataClick(View v) {
+		// back to last activity
+		setResult(RESULT_CANCELED);
+		finish();
+	}
+
 	/**
-	 * This method populates an activity which contains the profile of a student with information from the locally stored database
+	 * This method populates an activity which contains the profile of a student
+	 * with information from the locally stored database
 	 */
-	public void populateFields(){
+	public void populateFields() {
 		TextView nameField = (TextView) findViewById(R.id.student_name_field);
-		nameField.setText(curStudent.getLastName()+", "+curStudent.getFirstName());
+		nameField.setText(curStudent.getLastName() + ", "
+				+ curStudent.getFirstName());
 
 		TextView lastActionText = (TextView) findViewById(R.id.last_action_field);
-		lastActionText.setText( lastAction() );
+		lastActionText.setText(lastAction());
 
-		EditText gradeField = (EditText) findViewById(R.id.student_grade_field);
-		gradeField.setText(curStudent.getGrade()+"");
+		TextView gradeField = (TextView) findViewById(R.id.student_grade_field);
+		gradeField.setText(curStudent.getGrade() + "");
 
-		EditText phoneContField = (EditText) findViewById(R.id.phone_contact_field);
+		TextView phoneContField = (TextView) findViewById(R.id.phone_contact_field);
 		phoneContField.setText(curStudent.getPhone());
-		
-		EditText contactField = (EditText) findViewById(R.id.contact_relation_field);
+
+		TextView contactField = (TextView) findViewById(R.id.contact_relation_field);
 		contactField.setText(curStudent.getContactRelation());
-		
-		EditText contactRelField = (EditText) findViewById(R.id.contact_field);
+
+		TextView contactRelField = (TextView) findViewById(R.id.contact_field);
 		contactRelField.setText(curStudent.getContact());
-	
-		EditText addressContField = (EditText) findViewById(R.id.address_contact_field);
+
+		TextView addressContField = (TextView) findViewById(R.id.address_contact_field);
 		addressContField.setText(curStudent.getAddress());
 
 	}
@@ -82,25 +100,26 @@ public class StudentDataActivity extends SyncableActivity{
 
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-		if (lastAction!=null && !lastAction.defaultState() ) {
-			String activityName = activityData.get( lastAction.getActivityID() ).toString();
-			if ( lastAction.checkedIn() )
-				lastActionString = "Checked in to "+activityName+" "+dateFormat.format(lastAction.getInTime());
-			else if ( lastAction.checkedOut() )
-				lastActionString = "Checked out of "+activityName+" "+dateFormat.format(lastAction.getOutTime());
+		if (lastAction != null && !lastAction.defaultState()) {
+			String activityName = activityData.get(lastAction.getActivityID())
+					.toString();
+			if (lastAction.checkedIn())
+				lastActionString = "Checked in to " + activityName + " "
+						+ dateFormat.format(lastAction.getInTime());
+			else if (lastAction.checkedOut())
+				lastActionString = "Checked out of " + activityName + " "
+						+ dateFormat.format(lastAction.getOutTime());
 		}
 		return lastActionString;
 
 	}
 
-	public void onPause()
-	{
+	public void onPause() {
 		super.onPause();
 		closeData();
 	}
 
-	public void openData()
-	{
+	public void openData() {
 		studentData = new StudentDataSource(this);
 		studentData.open();
 		checkinData = new CheckinDataSource(this);
@@ -109,8 +128,7 @@ public class StudentDataActivity extends SyncableActivity{
 		activityData.open();
 	}
 
-	public void closeData()
-	{
+	public void closeData() {
 		studentData.close();
 		checkinData.close();
 		activityData.close();
