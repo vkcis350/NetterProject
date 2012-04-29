9e54b7df155328251e5d4f4380f395ff6caafcef
Xiao Li <lixiao1@seas.upenn.edu>
2012-04-29 15:40:28 -0400

    Robotium test for login screen.

--- AttendanceApp/src/edu/upenn/cis350/tests/AttendanceAppActivityTest.java
+++ AttendanceApp/src/edu/upenn/cis350/tests/AttendanceAppActivityTest.java
@@ -1,17 +1,66 @@
 package edu.upenn.cis350.tests;
 
+import com.jayway.android.robotium.solo.Solo;
+
 import edu.upenn.cis350.AttendanceAppActivity;
+import edu.upenn.cis350.R;
+import edu.upenn.cis350.R.id;
+import edu.upenn.cis350.models.SchoolActivity;
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class AttendanceAppActivityTest extends
-ActivityInstrumentationTestCase2<AttendanceAppActivity>
+	ActivityInstrumentationTestCase2<AttendanceAppActivity>
 {
-	public AttendanceAppActivityTest(Class<AttendanceAppActivity> activityClass) 
+
+
+	private Activity activity;
+	private Solo solo;
+	
+	public AttendanceAppActivityTest(String name) 
 	{
-		super(activityClass);
-		
+		super("edu.upenn.cis350",AttendanceAppActivity.class);
+		setName(name);
 	}
+	
+	
+	
+	
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		 solo = new Solo(getInstrumentation(), getActivity());
+		activity = getActivity();
+	}
+	 
+	 public void testDisplayLogin() throws Exception 
+	 {
+		 assertTrue(solo.searchText("Username"));
+		 assertTrue(solo.searchText("Password"));
+		 assertTrue(solo.searchButton("Login"));
+	 }
+
+	 public void testLoginFail() throws Exception
+	 {
+		 EditText userText = (EditText) solo.getView(R.id.user);
+		 EditText passwordText = (EditText) solo.getView(R.id.password);
+		 solo.enterText(userText, "user");
+		 solo.enterText(passwordText, "2");
+		 solo.clickOnButton("Login");
+		 assertTrue(solo.searchText("Login Failed"));
+	 }
+	 
+	 public void testLoginSuccess() throws Exception
+	 {
+		 EditText userText = (EditText) solo.getView(R.id.user);
+		 EditText passwordText = (EditText) solo.getView(R.id.password);
+		 solo.enterText(userText, "user");
+		 solo.enterText(passwordText, "1");
+		 solo.clickOnButton("Login");
+		 assertTrue(solo.searchButton("View Activities"));
+	 }
+	 
 }
